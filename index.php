70826ec33c36a926de969a881ee49c016692d25b
ram.mart17 <ram_mart1994@yahoo.com>
2019-09-25 18:20:21 +0800

    updated form box

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
             <div class="hero-body">
                 <div class="container">
                     <div class="columns is-centered">
-                        <div class="column is-12-tablet is-12-desktop is-12-widescreen">
+                        <div class="column is-12-tablet is-10-desktop">
                             <form class="box" method="POST">
 
                                 <b-field grouped>
